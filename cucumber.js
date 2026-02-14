854219534985d3d76e2bc05877710f42104ee2e8
Arvin <arvinsulimto@gmail.com>
2026-02-14 08:34:16 +0700

    move feature to src

--- cucumber.js
+++ cucumber.js
@@ -2,7 +2,7 @@ module.exports = {
   default: {
     requireModule: ['ts-node/register'],
     require: ['src/steps/*.ts', 'src/utils/hooks.ts'],
-    paths: ['features/*.feature'],
+    paths: ['src/features/*.feature'],
     format: ['progress-bar'],
     formatOptions: { snippetInterface: 'async-await' },
     worldParameters: {
