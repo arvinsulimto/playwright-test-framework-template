e9bba9030031f0c0381441429ef7ef3f41b4a9a3
Arvin <arvinsulimto@gmail.com>
2026-02-17 19:28:43 +0700

    delete unused

--- src/step-definitions/cart.steps.ts
+++ src/step-definitions/cart.steps.ts
@@ -7,9 +7,6 @@ import { page } from '../support/hooks';
 let inventoryPage: InventoryPage;
 let cartPage: CartPage;
 
-
-
-
 When('I add {string} to the cart', async (itemName: string) => {
     inventoryPage = new InventoryPage(page);
     await inventoryPage.addItemToCart(itemName);
@@ -21,7 +18,7 @@ Then('the cart badge should verify {string}', async (count: string) => {
 
 Then('I should see {string} in the cart', async (itemName: string) => {
     await page.locator('.shopping_cart_link').click();
-    
+
     cartPage = new CartPage(page);
     await expect(cartPage.cartItemName(itemName)).toBeVisible();
 });
